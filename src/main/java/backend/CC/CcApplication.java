dee6b656470d35ec1dc089fd86e3b94cb4451969
howudong <tjdvy963@naver.com>
2024-01-28 15:19:59 +0900

    chore : base setting springboot project

---
+++ src/main/java/backend/CC/CcApplication.java
@@ -0,0 +1,13 @@
+package backend.CC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcApplication.class, args);
+	}
+
+}
