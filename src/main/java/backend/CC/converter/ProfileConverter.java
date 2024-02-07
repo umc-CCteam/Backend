1417f51651cd342a8c8789ed8f79a4d7dddc2086
yerimya <syl1602@naver.com>
2024-02-08 01:14:43 +0900

    feat: #10 profileConverter

--- src/main/java/backend/CC/converter/ProfileConverter.java
+++ src/main/java/backend/CC/converter/ProfileConverter.java
@@ -1,11 +1,20 @@
 package backend.CC.converter;
 
+import backend.CC.domain.Profile;
 import backend.CC.web.dto.ProfileResponseDTO;
 
 public class ProfileConverter {
 
-
-    public static ProfileResponseDTO.ProfileDTO toProfile() {
+    public static ProfileResponseDTO.ProfileDTO toProfile(Profile profile) {
+        return ProfileResponseDTO.ProfileDTO.builder()
+                .profileId(profile.getId())
+                .nickname(profile.getName())
+                .email(profile.getEmail())
+                .comment(profile.getComment())
+                .photo(profile.getPhoto())
+                .googleAuth(profile.isGoogleAuth())
+                .instaAuth(profile.isInstaAuth())
+                .build();
 
     }
 }
